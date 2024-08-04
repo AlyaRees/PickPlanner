f1f6b877ec01d9de374a0dacf3915c1dbdeecc60
Alya Rees <alyarees@gmail.com>
2024-08-04 12:55:43 +0100

    Started home page for mobile

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function updateTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const currentTime = `${hours}:${minutes}:${seconds}`;
+    const currentTime = `${hours}:${minutes}`;
     
     document.getElementById('current-time').textContent = currentTime;
     // document.getElementById('last-updated').textContent = currentTime;
