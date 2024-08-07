fa4378df504305a68efd5deb0ff9cb2c8145c5e2
Alya Rees <alyarees@gmail.com>
2024-08-07 13:13:46 +0100

    Added functionality to add number to pick target inout field yay

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 function updateTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
@@ -11,28 +13,19 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 
-document.addEventListener('DOMContentLoaded', updateTime);
 
-// Extracts, processes and displays number of employees from inputted excel data
-
-// Gets pasted data from textarea form id="excelForm" from html
-// Adds event listener for submit button (<button type="submit"...) to run the function
-document.getElementById('excelForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Gets input data (.value) from <textarea id="excelData"...
-    // assigns it to name 'data' 
-    // and .trim() removes whitespace from both sides of a string
-    const data = document.getElementById('excelData').value.trim();
-    const rows = data.split('/n');
-    const rowCount = rows.length;
-
-    localStorage.setItem('rowCount', rowCount);
-
-    window.location.href = 'chill.html';
+const pickTarget = localStorage.getItem('pickTarget');
+if (pickTarget) {
+    document.getElementById('pick-target').textContent = pickTarget;
+}
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const rowCount = localStorage.getItem('rowCount');
-    document.getElementById('rowCount').textContent = rowCount;
+document.getElementById('inputForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const pickTarget = document.getElementById('employeeData').value.trim();
+
+    localStorage.setItem('pickTarget', pickTarget);
+
+    window.location.href = 'chill.html';
 });
