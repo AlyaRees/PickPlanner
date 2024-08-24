187b54012af942d7dcbad0fe557d0d388939a4fe
Alya Rees <alyarees@gmail.com>
2024-08-24 14:28:35 +0100

    Started adding function to handle input from mobile version

--- js/chill.js
+++ js/chill.js
@@ -203,6 +203,35 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    const editPageSubmitBtnMobile = document.getElementById('submit-data-mobile');
+    
+    if (editPageSubmitBtnMobile) {
+        editPageSubmitBtnMobile.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const pickTargetMobile = localStorage.getItem('pick-target-ta').value.trim();
+            const amountPickedMobile = localStorage.getItem('amount-picked-ta').value.trim();
+            const pickPerfNumOfEmployeesMobile = localStorage.getItem('pp-num-employees').value.trim();
+            const pickPerfCasesPerHrMobile = localStorage.getItem('pp-cases-per-hr').value.trim();
+
+            // Store the input values in localStorage
+            if (pickTargetMobile) {
+                localStorage.setItem('pickTarget', pickTargetMobile);
+            }
+            if (amountPickedMobile) {
+                localStorage.setItem('amount-picked-output', amountPickedMobile);
+            }
+            if (pickPerfNumOfEmployeesMobile) {
+                localStorage.setItem('numberOfEmployees', pickPerfNumOfEmployeesMobile);
+            }
+            if (pickPerfCasesPerHrMobile) {
+                localStorage.setItem('averageCasesPerHour', pickPerfCasesPerHrMobile);
+            }
+
+            // Optionally redirect to chill.html or another page
+            window.location.href = 'chill.html';
+        });
+    }
+
     const pickTargetOutput = localStorage.getItem('pickTarget');
     const amountPicked = localStorage.getItem('amount-picked-output');
     const numberOfEmployees = localStorage.getItem('numberOfEmployees');
